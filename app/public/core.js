69a6ffa288334c43ae4985ba1df6dcff15af6560
selmo <selmo@bluetext.com>
2018-02-19 20:03:05 -0500

    logic completed

--- app/public/core.js
+++ app/public/core.js
@@ -2,49 +2,100 @@
 var haystaxInterview = angular.module('haystaxInterview', []);
 var bearertoken = '';
 
-function mainController($scope, $http) {
+function loginController($scope, $http) {
+    $scope.showform = true;
+    $scope.showthank = false;
     $scope.formData = {};
 
-    // when landing on the page, get all todos and show them
-    $http.get('/api/user')
+    // when submitting the add form, send the text to the node API
+    $scope.userReg = function() {
+        console.log($scope.formData);
+        $http.post('/api/user', $scope.formData)
+            .success(function(data) {
+            $scope.showform = false;   
+            $scope.showthank = true;
+            console.log($scope.formData);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+}
+
+function mongoController($scope, $http) {
+    $scope.stepOne = true;
+    $scope.stepTwo = false;  
+    $scope.stepThree = false;  
+    $scope.stepFour = false;  
+    
+    // when landing on the page, get all data
+    $http.get('/api/data')
         .success(function(data) {
-            $scope.users = data;
+        $scope.mongoCollections = data;
+            console.log($scope.mongoCollections );
+    })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+    
+//Allow User to Add data through front end
+    $scope.addData = function() {
+    $scope.formData.wordList = $scope.formData.wordList.split(/[ ,.]+/); 
+    console.log($scope.formData);
+        $http.post('/api/add', $scope.formData)
+            .success(function(data) {
+            console.log($scope.formData);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+    
+    
+//This will get data on Trends.    
+$scope.getWordCount = function() {
+  $http.get('/api/count')
+        .success(function(data) {
+            $scope.stepOne = false;   
+            $scope.stepTwo = true;
             console.log(data);
+            $scope.words = data;
         })
         .error(function(data) {
             console.log('Error: ' + data);
         });
-
-    // when submitting the add form, send the text to the node API
-    $scope.userReg = function() {
-        $http.post('/api/user', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
     };
-
-    // delete a todo after checking it
-    $scope.deleteTodo = function(id) {
-        $http.delete('/api/user/' + id)
-            .success(function(data) {
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
+    
+$scope.getTrends = function() {
+  $http.get('/api/trends')
+        .success(function(data) {
+            $scope.stepThree = true;   
+            $scope.stepTwo = false;
+            console.log(data);
+            $scope.trends = data;
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
     };
-
+    
+$scope.getFinal = function() {
+  $http.get('/api/final')
+        .success(function(data) {
+            $scope.stepThree = false;   
+            $scope.stepFour = true;
+            console.log(data);
+            $scope.finals = data;
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+    };
+    
 }
 
 function twitterController($scope, $http) {
     
-        
     $scope.twitterSearch = {};
 
     // when landing on the page, get all todos and show them
@@ -53,55 +104,10 @@ function twitterController($scope, $http) {
         .success(function(data) {
             console.log(data);
             $scope.tweets = data.statuses;
-
         })
         .error(function(data) {
             console.log('Error: ' + data);
         });
-    };
-    
-     $scope.getLatestTweets = function() {
-            //create a deferred object using Angular's $q service
-            var deferred = $q.defer();
-            var url = 'https://api.twitter.com/1.1/search/tweets.json?';
-            if (data) {
-                url += '?q=' +  $scope.twitterSearch.handle;
-            }
-            var promise = authorizationResult.get(url).done(function(data) {
-                // https://dev.twitter.com/docs/api/1.1/get/statuses/home_timeline
-                // when the data is retrieved resolve the deferred object
-                deferred.resolve(data);
-            }).fail(function(err) {
-                deferred.reject(err);
-            });
-            //return the promise of the deferred object
-            return deferred.promise;
-        };
-        
- 
-    // when submitting the add form, send the text to the node API
-    $scope.userReg = function() {
-        $http.post('/api/user', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
-
-    // delete a todo after checking it
-    $scope.deleteTodo = function(id) {
-        $http.delete('/api/user/' + id)
-            .success(function(data) {
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
     };
 
 }
--- app/server.js
+++ app/server.js
@@ -1,41 +1,67 @@
 // server.js
     var express = require('express');
     var app = express();  
-    var mongoose = require('mongoose'); 
+    var mongoose = require('mongoose'); //Use Mongoose to interact with MongoDB
+
     var morgan = require('morgan'); 
     var bodyParser = require('body-parser'); 
     var methodOverride = require('method-override');
     var request = require('request'); 
-    var checkWord = require('check-word'),
-    words     = checkWord('en'); // setup the language for check, default is en 
+
+    var checkWord = require('check-word'), //Node Module for word checking
+    words     = checkWord('en'); // setup the language for counting english words in tweet.
     
-    var bearertoken='';
+    var bearertoken=''; //bearerToken will be set when initializing API call to Twitter.
 
-    mongoose.connect('mongodb://localhost:27017/haystax-interview'); 
+    mongoose.connect('mongodb://localhost:27017/haystax-interview');  //Connect to MongoDB
 
-    app.use(express.static(__dirname + '/public'));       
+    app.use(express.static(__dirname + '/public')); //Direct to client side directory.       
     app.use(morgan('dev'));                                     
     app.use(bodyParser.urlencoded({'extended':'true'}));      
     app.use(bodyParser.json());                               
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
     app.use(methodOverride());
 
- // define model =================
+ // define model for all colelctions used
     var User = mongoose.model('User', {
         email : String,
         password : String,
         phone : String
     });
 
-    var Tweet;
+    var str = mongoose.model('str', {
+        date : Date,
+        wordlist : Array,
+    });
 
-// routes ======================================================================
+    var recentWordCount = mongoose.model('recentWordCount', {
+        _id : String,
+        value : Number
+    },'recentWordCounts');
 
-    // api ---------------------------------------------------------------------
-    // get all todos
+    var oldWordCount = mongoose.model('oldWordCount', {
+        _id : String,
+        value : Number
+    },'oldWordCounts');
+
+    var totalWordCount = mongoose.model('totalWordCount', {
+        _id : String,
+        value : Number
+    },'totalWordCounts');
+
+    var trendingWordCount = mongoose.model('trendingWord', {
+        _id : String,
+        value : Number
+    },'trendingWords');
+
+
+
+// API Section
+
+    // get all Users
     app.get('/api/user', function(req, res) {
-
-        // use mongoose to get all todos in the database
+        
+        // use mongoose to get all Users in the database
         User.find(function(err, user) {
 
             // if there is an error retrieving, send the error. nothing after res.send(err) will execute
@@ -46,7 +72,173 @@
         });
     });
 
+//Get Word lists from database
+    app.get('/api/data', function(req, res) {
+        
+            str.find(function(err, str) {
+                if (err){
+                    res.send(err)
+                }
+//Here we need to MapReduce for 3 scenarios - Total word counts, 24 hour word count, and previous 24 hour word count.
+                recentWords();
+                oldWords();
+                allWords();
+//Here we respond with the data set - after we generate collections for data parsing. 
+                res.json(str);
+            });
+    });
+
+    // create user and send back all users after creation
+    app.post('/api/add', function(req, res) {
+        // create a todo, information comes from AJAX request from Angular
+        str.create({
+            date : req.body.date,
+            wordlist : req.body.wordList,
+        }, function(err, user) {
+            if (err)
+                res.send(err);
+
+            // get and return all the wordlists after you create another
+            str.find(function(err, str) {
+                if (err){
+                    res.send(err)
+                }
+//Here we need to MapReduce for 3 scenarios - Total word counts, 24 hour word count, and previous 24 hour word count.
+                recentWords();
+                oldWords();
+                allWords();
+//Here we respond with the data set - after we generate collections for data parsing. 
+                res.json(str);
+            });
+        });
+
+    });
+
+//This call will initiate data parsing. 
+
+    app.get('/api/count', function(req, res) {
+
+            totalWordCount.find(function(err, totalWordCount) {
+                if (err){
+                    res.send(err)
+                }
+                res.json(dataSort(totalWordCount));
+            });
+    });
+
+//Now we sort the most recent word counts
+    app.get('/api/trends', function(req, res) {
+
+      recentWordCount.find(function(err, recentWordCount) {
+                if (err){
+                    res.send(err)
+                }
+           trendingWords()
+            res.json(dataSort(recentWordCount));
+            });
+        
+    });
+
+//Now we return the trending word list
+    app.get('/api/final', function(req, res) {
+
+      trendingWordCount.find(function(err, trendingWordCount) {
+                if (err){
+                    res.send(err)
+                }
+            res.json(dataSort(trendingWordCount));
+            });
+        
+    });
+
+
+//This will sort the data in descending word count. 
+function dataSort(data){
+data.sort(function(a, b) { return b.value - a.value; });
+
+console.log(data);
+return data;
+}
+
+
+function recentWords(){
+ var o = {};
+o.map = function () { 
+    var d = new Date();
+    d.setDate(d.getDate() - 1); 
+    if(this.date>=d){
+    for(var i =0; i< this.wordlist.length - 1; i++){
+    emit(this.wordlist[i], 1)
+    }
+    } 
+}
+
+o.out = {replace: 'recentWordCounts'}
+
+o.reduce = function (k, vals) { return vals.length }
+str.mapReduce(o, function (err, results) {
+})
+
+}
+
+function oldWords(){
+ var o = {};
+o.map = function () { 
+    var d2 = new Date();
+    var d3 = new Date();
+
+    d2.setDate(d2.getDate() - 1); 
+    d3.setDate(d3.getDate() - 2); 
+                
+    if(this.date<=d2&&this.date<=d3){
+    for(var i =0; i< this.wordlist.length - 1; i++){
+    emit(this.wordlist[i], 1)
+    }
+    }
+}
+o.out = {replace: 'oldWordCounts'}
+o.reduce = function (k, vals) { return vals.length }
+str.mapReduce(o, function (err, results) {
+})
+}
+
+function allWords(){
+ var o = {};
+o.map = function () { 
+    for(var i =0; i< this.wordlist.length - 1; i++){
+    emit(this.wordlist[i], 1)} }
+o.out = {replace: 'totalWordCounts'}
+
+o.reduce = function (k, vals) { return vals.length } 
+str.mapReduce(o, function (err, results) {
+})
+}
+
+//Now we reduce the 2 collections subtracting 24 hours word count, with previous 24 hour word count and output to a collection
+function trendingWords(){
+trendingWordCount.remove({}, function(err) { 
+   console.log('collection removed') 
+});
+ var o = {};
+o.map = function () { 
+    emit(this._id, this.value)}
+o.out = {reduce: 'trendingWords'}
+
+o.reduce = function (k, vals) {
+    var values = 0;
+    values +=vals[1];
+    values -=vals[0]
+return values;
+}
+recentWordCount.mapReduce(o, function (err, results) {
+})
+oldWordCount.mapReduce(o, function (err, results) {
+})
+}
+
+
     app.get('/api/twitter', function(req, res) {
+        
        var options = {
     url: 'https://api.twitter.com/1.1/search/tweets.json?q='+req.body.handle+'&count=5',
     headers: {
@@ -80,9 +272,8 @@
 
 
 
-    // create todo and send back all todos after creation
+    // create user and send back all users after creation
     app.post('/api/user', function(req, res) {
-
         // create a todo, information comes from AJAX request from Angular
         User.create({
             email : req.body.email,
@@ -104,30 +295,22 @@
 
 
 
-    // delete a todo
-    app.delete('/api/user/:user_id', function(req, res) {
-        User.remove({
-            _id : req.params.user_id
-        }, function(err, user) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            User.find(function(err, user) {
-                if (err)
-                    res.send(err)
-                res.json(users);
-            });
-        });
-    });
 
     // application -------------------------------------------------------------
     app.get('/register', function(req, res) {
-        res.sendfile('./public/register.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendfile('./public/register.html');
     });
+
+    app.get('/mongo', function(req, res) {
+        res.sendfile('./public/mongo.html'); 
+    });
+
+
     app.get('/', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendfile('./public/index.html'); 
     });
+
+
    app.get('/twitter', function(req, res) {
        var consumerkey = 'eNqhaQZ87zJtdfMA9fznEHy3i';
        var consumersecret = '4RWRmPkNkliikw17n05HmPFjgCg9vFhctvS3KVmtfuqjMWFDmB';
